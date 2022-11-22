e3c48b5bb15e34b8d69fe32d3b1ae3afecbca36c
MRFLEX22 <118789168+MRFLEX22@users.noreply.github.com>
2022-11-22 06:24:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
+global.Owner = ['276049719']
+global.OwnerNumber = ['672049719']
 global.ownertag = ['918101187835']
 global.BotName = "Miku"
-global.packname = "Miku bot"
+global.packname = "Hata bot"
 global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.OwnerName = "22vyb"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
